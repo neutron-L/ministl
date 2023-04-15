ff38c1ce882213efbab23887aa65bbd053707676
rda <2055017970@qq.com>
2023-04-15 22:07:51 +0800

    add and implement test traits

--- tests/test_traits.cc
+++ tests/test_traits.cc
@@ -1,11 +1,56 @@
 #include <iostream>
+#include <cassert>
+#include "type_traits.hh"
 
 using std::cin;
 using std::cout;
 using std::endl;
 
+struct Item
+{
+};
+
+void test_self_define_type_traits()
+{
+    assert(typeid(stl::type_traits<Item>::has_trivial_default_constructor) == typeid(stl::__false_type));
+    assert(typeid(stl::type_traits<Item>::has_trivial_copy_constructor) == typeid(stl::__false_type));
+    assert(typeid(stl::type_traits<Item>::has_trivial_assignment_operator) == typeid(stl::__false_type));
+    assert(typeid(stl::type_traits<Item>::has_tirvial_destructor) == typeid(stl::__false_type));
+    assert(typeid(stl::type_traits<Item>::is_POD_type) == typeid(stl::__false_type));
+}
+
+template <typename T>
+void test_basic_type_traits()
+{
+    assert(typeid(typename stl::type_traits<T>::has_trivial_default_constructor) == typeid(stl::__true_type));
+    assert(typeid(typename stl::type_traits<T>::has_trivial_copy_constructor) == typeid(stl::__true_type));
+    assert(typeid(typename stl::type_traits<T>::has_trivial_assignment_operator) == typeid(stl::__true_type));
+    assert(typeid(typename stl::type_traits<T>::has_tirvial_destructor) == typeid(stl::__true_type));
+    assert(typeid(typename stl::type_traits<T>::is_POD_type) == typeid(stl::__true_type));
+}
+
+template <typename T>
+void test_pointer_type_traits()
+{
+    assert(typeid(typename stl::type_traits<T *>::has_trivial_default_constructor) == typeid(stl::__true_type));
+    assert(typeid(typename stl::type_traits<T *>::has_trivial_copy_constructor) == typeid(stl::__true_type));
+    assert(typeid(typename stl::type_traits<T *>::has_trivial_assignment_operator) == typeid(stl::__true_type));
+    assert(typeid(typename stl::type_traits<T *>::has_tirvial_destructor) == typeid(stl::__true_type));
+    assert(typeid(typename stl::type_traits<T *>::is_POD_type) == typeid(stl::__true_type));
+}
+
 int main()
 {
-    
+    test_self_define_type_traits();
+
+    test_basic_type_traits<int>();
+    test_basic_type_traits<char>();
+    // self-define type pointer
+    test_pointer_type_traits<Item>();
+    // basic type pointer
+    test_pointer_type_traits<double>();
+    // const pointer
+    test_pointer_type_traits<const int>();
+
     return 0;
 }
