6d19e0357096ed5df2ae28409f43b6f43d40a2c0
rda <2055017970@qq.com>
2023-05-14 18:57:56 +0800

    define some user-defined types for test

---
+++ tests/type.hh
@@ -0,0 +1,45 @@
+#ifndef MINISTL_TYPE_HH
+#define MINISTL_TYPE_HH
+
+#include <iostream>
+
+struct Item
+{
+    void * ptr;
+
+    Item()=default;
+    Item(const Item & rhs)
+    {
+        ptr = rhs.ptr;
+        std::cout << "copy-constructor" << std::endl;
+    }
+
+    Item(Item && rhs)
+    {
+        ptr = rhs.ptr;
+        rhs.ptr = nullptr;
+        std::cout << "move-constructor" << std::endl;
+    }
+
+    Item & operator=(const Item & rhs)
+    {
+        if (this != &rhs)
+            ptr = rhs.ptr;
+        std::cout << "copy-assignment" << std::endl;
+        return *this;
+    }
+
+    Item & operator=(Item && rhs)
+    {
+        if (this != &rhs)
+        {
+            ptr = rhs.ptr;
+            rhs.ptr = nullptr;
+            std::cout << "move-assginment" << std::endl;
+        }
+        
+        return *this;
+    }
+};
+
+#endif
