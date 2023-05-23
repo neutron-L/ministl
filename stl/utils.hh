dcd79da158ccbee357ab1621701f29a46a72f7df
rda <2055017970@qq.com>
2023-05-23 20:05:56 +0800

    add utils.hh(contains log macro

---
+++ stl/utils.hh
@@ -0,0 +1,14 @@
+/* Logging */
+
+#ifdef DEBUG
+#define debug(M, ...) \
+    fprintf(stderr, "DEBUG %s:%d:%s: " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#else
+#define debug(M, ...)
+#endif
+
+#define info(M, ...) \
+    fprintf(stderr, "INFO  " M "\n", ##__VA_ARGS__)
+
+#define error(M, ...) \
+    fprintf(stderr, "ERROR " M "\n", ##__VA_ARGS__)
