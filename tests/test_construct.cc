98da7fe353a9657c638c9ad36ff6e0a2d017486c
rda <2055017970@qq.com>
2023-05-23 19:41:10 +0800

    add construct test cases

--- tests/test_construct.cc
+++ tests/test_construct.cc
@@ -1,8 +1,12 @@
 #include <cassert>
 #include <vector>
+#include "complex.hh"
+#include "string.hh"
 #include "array.hh"
 #include "construct.hh"
 #include "type_traits.hh"
+#include "string.hh"
+#include "complex.hh"
 
 // built-in type
 void test_built_in_type()
@@ -28,44 +32,104 @@ void test_user_defined_type()
 {
     printf("===========%s===========\n", __FUNCTION__);
 
-    struct Obj
-    {
-        int i{};
-        double d{};
-        Obj() = default;
-        Obj(int ii, double dd) : i(ii), d(dd) {}
-        ~Obj() { std::cout << "Obj destructor\n"; }
-    };
+    std::cout << "construct one string (default)\n";
+    String *p = (String *)malloc(sizeof(String));
+    char *str1 = "This is string\n";
+    stl::construct(p, str1);
+    assert(!strcmp(p->get_c_str(), str1));
 
-    std::cout << "test destroy one obj\n";
-    Obj *p = (Obj *)malloc(sizeof(Obj));
-    stl::construct(p, 1, 3.14);
-    assert(p->i == 1);
-    assert(p->d == 3.14);
-
+    std::cout << "destroy\n";
     stl::destroy(p);
+
+    char *str2 = "This is second\n";
+    String so(str2);
+    std::cout << "--------constructo one string (copy)-------\n";
+    stl::construct(p, so);
+    assert(!strcmp(p->get_c_str(), str2));
+    std::cout << "destroy\n";
+    stl::destroy(p);
+    std::cout << "--------constructo one string (std::move)-------\n";
+    stl::construct(p, std::move(so));
+    assert(so.get_c_str() == nullptr);
+    assert(!strcmp(p->get_c_str(), str2));
+    std::cout << "destroy\n";
+    stl::destroy(p);
+
     free(p);
 
-    std::cout << "test destroy range in array\n";
+    std::cout << "------destroy a stl::array of complex-------\n";
     // destroy stl::array
-    stl::array<Obj, 4> ao{Obj(1, 2), Obj(3, 4), Obj(5, 6), Obj(7, 8)};
-    stl::destroy<typename stl::array<Obj, 4>::iterator>(ao.begin(), ao.end());
+    stl::array<complex, 4> ao{complex(1, 2), complex(3, 4), complex(5, 6), complex(7, 8)};
+    stl::destroy<typename stl::array<complex, 4>::iterator>(ao.begin(), ao.end());
 
     // destroy heap-allocated array of objs
-    std::cout << "test destroy range in array\n";
+    std::cout << "------destroy a c-array of complex-------\n";
     int num = 10;
-    Obj *po = (Obj *)malloc(sizeof(Obj) * num);
+    complex *po = (complex *)malloc(sizeof(complex) * num);
     for (int i = 0; i < num; ++i)
-        stl::construct(&po[i]);
+        stl::construct(&po[i], i, i + 1);
+    for (int i = 0; i < num; ++i)
+        assert(po[i].real() == i && po[i].imag() == i + 1);
     stl::destroy(&po[0], &po[num]);
     free(po);
     std::cout << "==========================\n";
+}
+
+// mix user-define type test
+struct Mix
+{
+    String str{};
+    complex cmx{};
+
+    Mix() = default;
+    Mix(const String &s, const complex &c) : str(s), cmx(c)
+    {
+    }
+    Mix(String &&s, const complex &c) : str(std::move(s)), cmx(c)
+    {
+    }
+    Mix(const String &s, complex &&c) : str(s), cmx(std::move(c))
+    {
+    }
+    Mix(String &&s, complex &&c) : str(std::move(s)), cmx(std::move(c))
+    {
+    }
+};
+
+void test_mix_user_defined_type()
+{
+    printf("===========%s===========\n", __FUNCTION__);
+    Mix * mix = (Mix *)malloc(sizeof(Mix));
+
+    std::cout << "---test Mix(const String & s, const complex & c)----\n";
+    String s1("hello world");
+    complex c1(1.1, 3.4);
+    stl::construct(mix, s1, c1);
+    stl::destroy(mix);
+    std::cout << "---test Mix(String && s, const complex & c)----\n";
+    String s2("hello world");
+    complex c2(1.1, 3.4);
+    stl::construct(mix, std::move(s2), c2);
+    stl::destroy(mix);
+    std::cout << "---test Mix(const String & s, complex && c)----\n";
+    String s3("hello world");
+    complex c3(1.1, 3.4);
+    stl::construct(mix, s3, std::move(c3));
+    stl::destroy(mix);
+    std::cout << "---test Mix(String && s, complex && c)----\n";
+    String s4("hello world");
+    complex c4(1.1, 3.4);
+    stl::construct(mix, std::move(s4), std::move(c4));
+    stl::destroy(mix);
+    std::cout << "----------------------------\n";
 }
 
 int main()
 {
     test_built_in_type();
     test_user_defined_type();
+    test_mix_user_defined_type();
+    std::cout << "Pass!" << std::endl;
 
     return 0;
 }
