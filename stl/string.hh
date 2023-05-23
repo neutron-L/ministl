231390f62a329c096f40b435fb6cfbfd2dae99d5
rda <2055017970@qq.com>
2023-05-23 20:13:52 +0800

    add some debug info to string.hh

--- stl/string.hh
+++ stl/string.hh
@@ -3,63 +3,87 @@
 
 class String
 {
-public:                                 
-   String(const char* cstr=0);                     
-   String(const String& str);                    
-   String& operator=(const String& str);         
-   ~String();                                    
-   char* get_c_str() const { return m_data; }
+public:
+    String(const char *cstr = 0);
+    String(const String &str);
+    String(String &&str);
+    String &operator=(const String &str);
+    String &operator=(String &&str);
+    ~String();
+    char *get_c_str() const { return m_data; }
+
 private:
-   char* m_data;
+    char *m_data;
 };
 
 #include <cstring>
+#include <iostream>
+#include "utils.hh"
 
-inline
-String::String(const char* cstr)
+inline String::String(const char *cstr)
 {
-   if (cstr) {
-      m_data = new char[strlen(cstr)+1];
-      strcpy(m_data, cstr);
-   }
-   else {   
-      m_data = new char[1];
-      *m_data = '\0';
-   }
+    debug("String: default constructor");
+    if (cstr)
+    {
+        m_data = new char[strlen(cstr) + 1];
+        strcpy(m_data, cstr);
+    }
+    else
+    {
+        m_data = new char[1];
+        *m_data = '\0';
+    }
 }
 
-inline
-String::~String()
+inline String::~String()
 {
-   delete[] m_data;
+    debug("String:  destructor");
+    delete[] m_data;
 }
 
-inline
-String& String::operator=(const String& str)
+inline String &String::operator=(const String &str)
 {
-   if (this == &str)
-      return *this;
+    if (this == &str)
+        return *this;
 
-   delete[] m_data;
-   m_data = new char[ strlen(str.m_data) + 1 ];
-   strcpy(m_data, str.m_data);
-   return *this;
+    delete[] m_data;
+    m_data = new char[strlen(str.m_data) + 1];
+    strcpy(m_data, str.m_data);
+    return *this;
 }
 
-inline
-String::String(const String& str)
+inline String &String::operator=(String &&str)
 {
-   m_data = new char[ strlen(str.m_data) + 1 ];
-   strcpy(m_data, str.m_data);
+    if (this == &str)
+        return *this;
+
+    m_data = str.m_data;
+    str.m_data = nullptr;
+    return *this;
+}
+
+inline String::String(const String &str)
+{
+    debug("String: copy constructor");
+    m_data = new char[strlen(str.m_data) + 1];
+    strcpy(m_data, str.m_data);
+}
+
+inline String::String(String &&str)
+{
+    debug("String: move constructor");
+
+    m_data = str.m_data;
+    str.m_data = nullptr;
 }
 
 #include <iostream>
 using namespace std;
 
-ostream& operator<<(ostream& os, const String& str)
+ostream &operator<<(ostream &os, const String &str)
 {
-   os << str.get_c_str();
-   return os;
+    os << str.get_c_str();
+    return os;
 }
 
 #endif
