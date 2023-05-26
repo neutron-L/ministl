a4d6c374babdc0ad8b21cc31e2bc50449a02c35b
rda <2055017970@qq.com>
2023-05-26 16:48:13 +0800

    start numeric and algobase

---
+++ stl/algobase.hh
@@ -0,0 +1,14 @@
+//
+// Created by rda on 2023/5/26.
+//
+
+#ifndef MINISTL_ALGOBASE_HH
+#define MINISTL_ALGOBASE_HH
+
+namespace stl
+{
+
+} // namespace stl
+
+
+#endif
---
+++ stl/numeric.hh
@@ -0,0 +1,15 @@
+//
+// Created by rda on 2023/5/26.
+//
+
+#ifndef MINISTL_NUMERIC_HH
+#define MINISTL_NUMERIC_HH
+
+namespace stl
+{
+
+
+} // namespace stl
+
+
+#endif
