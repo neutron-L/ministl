de32097477cc269bf33eb7c02b41f759fcbd3961
rda <2055017970@qq.com>
2023-05-28 20:34:14 +0800

    add algobase.hh

--- stl/algobase.hh
+++ stl/algobase.hh
@@ -7,8 +7,88 @@
 
 namespace stl
 {
+    /* Modifying sequence operations */
+    template <typename InputIt, typename OutputIt>
+    OutputIt copy(InputIt first, InputIt last, OutputIt d_first)
+    {
+    }
+
+    template <typename InputIt, typename OutputIt, typename UnaryPredictate>
+    OutputIt copy(InputIt first, InputIt last, OutputIt d_first, UnaryPredictate pred)
+    {
+    }
+
+    template <typename BidirIt1, typename BidirIt2>
+    BidirIt2 copy_backward(BidirIt1 first, BidirIt1 last, BidirIt2 d_last)
+    {
+    }
+
+    template <typename InputIt, typename Size, typename OutputIt>
+    OutputIt copy_n(InputIt first, Size count, OutputIt result)
+    {
+    }
+
+    template <typename ForwardIt, typename T>
+    void fill(ForwardIt first, ForwardIt last, const T &value)
+    {
+    }
+
+    template <typename OutputIt, typename Size, typename T>
+    void fill_n(OutputIt first, Size count, const T &value)
+    {
+    }
+
+    template <class T>
+    void swap(T &a, T &b) noexcept
+    {
+        T tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    template <typename T, std::size_t N>
+    void swap(T (&a)[N], T (&b)[N])
+    {
+        for (std::size_t i = 0; i < N; ++i)
+            swap(a[i], b[i]);
+    }
+
+    template <typename ForwardIt1, typename ForwardIt2>
+    void iter_swap(ForwardIt1 a, ForwardIt2 b)
+    {
+        swap(*a, *b);
+    }
+
+    /* Comparison operations */
+    template <typename InputIt1, typename InputIt2>
+    bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2)
+    {
+    }
+
+    template <typename InputIt1, typename InputIt2>
+    bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2)
+    {
+    }
+    /* Minimum/maximum operations */
+    template <class T>
+    const T &max(const T &a, const T &b)
+    {
+        return (b < a) ? a : b;
+    }
+    template <class T>
+    const T &min(const T &a, const T &b)
+    {
+        return (b < a) ? b : a;
+    }
+
+    /* Non-modifying sequence operations */
+    template <class InputIt1, class InputIt2>
+    std::pair<InputIt1, InputIt2>
+    mismatch(InputIt1 first1, InputIt1 last1,
+             InputIt2 first2)
+    {
+    }
 
 } // namespace stl
-
 
 #endif
