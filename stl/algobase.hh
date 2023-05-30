688d3a424a41232bbc3d108a7b5f9f7709787059
rda <2055017970@qq.com>
2023-05-30 22:25:01 +0800

    implement some functions

--- stl/algobase.hh
+++ stl/algobase.hh
@@ -7,15 +7,55 @@
 
 namespace stl
 {
+    /* Minimum/maximum operations */
+    template <class T>
+    const T &max(const T &a, const T &b)
+    {
+        return (b < a) ? a : b;
+    }
+    template <class T>
+    const T &min(const T &a, const T &b)
+    {
+        return (b < a) ? b : a;
+    }
+
+    /* Non-modifying sequence operations */
+    template <class InputIt1, class InputIt2>
+    std::pair<InputIt1, InputIt2>
+    mismatch(InputIt1 first1, InputIt1 last1,
+             InputIt2 first2)
+    {
+        while (first1 != last1 && *first1 == *first2)
+        {
+            ++first1;
+            ++first2;
+        }
+
+        return {first1, first2};
+    }
+    
     /* Modifying sequence operations */
     template <typename InputIt, typename OutputIt>
     OutputIt copy(InputIt first, InputIt last, OutputIt d_first)
     {
+        while (first != last)
+            *d_first++ = *first++;
+        return d_first;
     }
 
     template <typename InputIt, typename OutputIt, typename UnaryPredictate>
     OutputIt copy(InputIt first, InputIt last, OutputIt d_first, UnaryPredictate pred)
     {
+        for (; first != last; ++first)
+        {
+            if (pred(*first))
+            {
+                *d_first = *first;
+                ++d_first;
+            }
+        }
+
+        return d_first;
     }
 
     template <typename BidirIt1, typename BidirIt2>
@@ -26,16 +66,23 @@ namespace stl
     template <typename InputIt, typename Size, typename OutputIt>
     OutputIt copy_n(InputIt first, Size count, OutputIt result)
     {
+        while (count--)
+            *result++ = *first++;
+        return result;
     }
 
     template <typename ForwardIt, typename T>
     void fill(ForwardIt first, ForwardIt last, const T &value)
     {
+        while (first != last)
+            *first++ = value;
     }
 
     template <typename OutputIt, typename Size, typename T>
     void fill_n(OutputIt first, Size count, const T &value)
     {
+        while (count--)
+            *first++ = value;
     }
 
     template <class T>
@@ -63,30 +110,68 @@ namespace stl
     template <typename InputIt1, typename InputIt2>
     bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2)
     {
+        while (first1 != last1 && *first1 == *first2)
+        {
+            ++first1;
+            ++first2;
+        }
+        return first1 == last1;
+    }
+
+    template <class InputIt1, class InputIt2, class BinaryPredicate>
+    bool equal(InputIt1 first1, InputIt1 last1,
+               InputIt2 first2, BinaryPredicate p)
+    {
+        while (first1 != last1 && p(*first1, *first2))
+        {
+            ++first1;
+            ++first2;
+        }
+
+        return first1 == last1;
     }
 
     template <typename InputIt1, typename InputIt2>
     bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2)
     {
-    }
-    /* Minimum/maximum operations */
-    template <class T>
-    const T &max(const T &a, const T &b)
-    {
-        return (b < a) ? a : b;
-    }
-    template <class T>
-    const T &min(const T &a, const T &b)
-    {
-        return (b < a) ? b : a;
+        while (first1 != last1 && first2 != last2)
+        {
+            if (*first1 < *first2)
+                return true;
+            else if (*first1 > *first2)
+                return false;
+            ++first1;
+            ++first2;
+        }
+
+        return first1 == last1 && first2 != last2;
     }
 
-    /* Non-modifying sequence operations */
-    template <class InputIt1, class InputIt2>
-    std::pair<InputIt1, InputIt2>
-    mismatch(InputIt1 first1, InputIt1 last1,
-             InputIt2 first2)
+    template <typename InputIt1, typename InputIt2, typename Compare>
+    bool lexicographical_compare(InputIt1 first1, InputIt1 last1,
+                                 InputIt2 first2, InputIt2 last2, Compare cmp)
     {
+        while (first1 != last1 && first2 != last2)
+        {
+            if (cmp(*first1, *first2))
+                return true;
+            else if (cmp(*first2, *first1))
+                return false;
+            ++first1;
+            ++first2;
+        }
+
+        return first1 == last1 && first2 != last2;
+    }
+
+    template <>
+    bool lexicographical_compare(const char * first1, const char * last1, const char * first2, const char * last2)
+    {
+        const int len1 = last1 - first1;
+        const int len2 = last2 - first2;
+        int result = strncmp(first1, first2, stl::min(len1, len2));
+        
+        return result != 0 ? result < 0 : len1 < len2;
     }
 
 } // namespace stl
