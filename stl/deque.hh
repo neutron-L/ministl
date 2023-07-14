f738ccecab8bf5013fb586abe4af44b82d550989
rda <2055017970@qq.com>
2023-07-14 14:34:05 +0800

    deque: start...

---
+++ stl/deque.hh
@@ -0,0 +1,31 @@
+//
+// Created by rda on 2023/7/14.
+//
+
+#ifndef MINISTL_DEQUE_HH
+#define MINISTL_DEQUE_HH
+
+#include <iostream>
+#include <cstddef>
+#include <initializer_list>
+#include <utility>
+#include <stdexcept>
+#include <cstring>
+#include <limits>
+
+#include "alloc.hh"
+#include "iterator.hh"
+#include "uninitialized.hh"
+#include "algobase.hh"
+
+namespace stl
+{
+    template <typename T, typename Alloc = alloc>
+    class deque
+    {
+
+    };
+    
+} // namespace stl
+
+#endif
