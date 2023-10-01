8295caa77eee8c6ba5f4560de621d7e9d9381488
rda <2055017970@qq.com>
2023-10-01 10:27:50 +0800

    algobase: add lower_bound and upper_bound

--- stl/algobase.hh
+++ stl/algobase.hh
@@ -9,6 +9,7 @@
 #include <cstring>
 
 #include "construct.hh"
+#include "iterator.hh"
 
 namespace stl
 {
@@ -38,7 +39,7 @@ namespace stl
 
         return {first1, first2};
     }
-    
+
     /* Modifying sequence operations */
     template <typename InputIt, typename OutputIt>
     OutputIt copy(InputIt first, InputIt last, OutputIt d_first)
@@ -98,7 +99,6 @@ namespace stl
         return first;
     }
 
-
     template <class T>
     void swap(T &a, T &b) noexcept
     {
@@ -120,7 +120,9 @@ namespace stl
         swap(*a, *b);
     }
 
-    /* Comparison operations */
+    /*
+     * Comparison operations
+     * */
     template <typename InputIt1, typename InputIt2>
     bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2)
     {
@@ -179,13 +181,126 @@ namespace stl
     }
 
     template <>
-    bool lexicographical_compare(const char * first1, const char * last1, const char * first2, const char * last2)
+    bool lexicographical_compare(const char *first1, const char *last1, const char *first2, const char *last2)
     {
         const int len1 = last1 - first1;
         const int len2 = last2 - first2;
         int result = strncmp(first1, first2, stl::min(len1, len2));
-        
+
         return result != 0 ? result < 0 : len1 < len2;
+    }
+
+    /*
+     * Search
+     * */
+    template <typename ForwardIt, typename T, typename Distance>
+    ForwardIt lower_bound_aux(ForwardIt first, ForwardIt last, const T &value, Distance *, forward_iterator_tag)
+    {
+        Distance len = stl::distance(first, last);
+        ForwardIt middle;
+        Distance half;
+
+        while (len > 0)
+        {
+            half = len >> 1;
+            middle = first;
+            stl::advance(middle, half);
+            if (*middle < value)
+            {
+                first = middle;
+                ++first;
+                len = len - half - 1;
+            }
+            else
+                len = half;
+        }
+
+        return first;
+    }
+
+    template <typename ForwardIt, typename T, typename Distance>
+    ForwardIt lower_bound_aux(ForwardIt first, ForwardIt last, const T &value, Distance *, random_access_iterator_tag)
+    {
+        Distance len = last - first;
+        ForwardIt middle;
+        Distance half;
+
+        while (len > 0)
+        {
+            half = len >> 1;
+            middle = first + half;
+            if (*middle < value)
+            {
+                first = middle;
+                ++first;
+                len = len - half - 1;
+            }
+            else
+                len = half;
+        }
+
+        return first;
+    }
+
+    template <typename ForwardIt, typename T>
+    inline ForwardIt lower_bound(ForwardIt first, ForwardIt last, const T &value)
+    {
+        return lower_bound_aux(first, last, value, stl::distance_type(first), stl::iterator_category(first));
+    }
+
+    template <typename ForwardIt, typename T, typename Distance>
+    ForwardIt upper_bound_aux(ForwardIt first, ForwardIt last, const T &value, Distance *, forward_iterator_tag)
+    {
+        Distance len = stl::distance(first, last);
+        ForwardIt middle;
+        Distance half;
+
+        while (len > 0)
+        {
+            half = len >> 1;
+            middle = first;
+            stl::advance(middle, half);
+            if (value < *middle)
+                len = half;
+            else
+            {
+                first = middle;
+                ++first;
+                len = len - half - 1;
+            }
+        }
+
+        return first;
+    }
+
+    template <typename ForwardIt, typename T, typename Distance>
+    ForwardIt upper_bound_aux(ForwardIt first, ForwardIt last, const T &value, Distance *, random_access_iterator_tag)
+    {
+        Distance len = last - first;
+        ForwardIt middle;
+        Distance half;
+
+        while (len > 0)
+        {
+            half = len >> 1;
+            middle = first + half;
+            if (value < *middle)
+                len = half;
+            else
+            {
+                first = middle;
+                ++first;
+                len = len - half - 1;
+            }
+        }
+
+        return first;
+    }
+
+    template <typename ForwardIt, typename T>
+    inline ForwardIt upper_bound(ForwardIt first, ForwardIt last, const T &value)
+    {
+        return upper_bound_aux(first, last, value, stl::distance_type(first), stl::iterator_category(first));
     }
 
 } // namespace stl
